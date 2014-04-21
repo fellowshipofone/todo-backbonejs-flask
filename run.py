e4237ecd33ca582729abe1fd51cd92f8a4abeff2
Nicolas Grasset <nicolas.grasset@gmail.com>
2014-04-21 20:30:18 +0200

    Initial commit

---
+++ run.py
@@ -0,0 +1,168 @@
+from flask import Flask, render_template, request
+from flask.ext.sqlalchemy import SQLAlchemy
+import json
+
+# Define the WSGI application object
+app = Flask(__name__)
+
+# Configurations
+app.config.from_object('config')
+
+# Define the database object which is imported
+# by modules and controllers
+db = SQLAlchemy(app)
+
+
+##
+##
+## Models
+##
+
+class Item(db.Model):
+    """
+    Todo items
+    """
+
+    __tablename__ = 'todo_item'
+
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.String(128),  nullable=False)
+    is_done = db.Column(db.Boolean, nullable=False)
+    order = db.Column(db.SmallInteger, nullable=False)
+    date_created  = db.Column(db.DateTime,  default=db.func.current_timestamp())
+    date_modified = db.Column(db.DateTime,  default=db.func.current_timestamp(),
+                                           onupdate=db.func.current_timestamp())
+
+    # New instance instantiation procedure
+    def __init__(self, task, is_done, order):
+        self.task = task
+        self.is_done = is_done
+        self.order = order
+
+    def __repr__(self):
+        return '<Item %r>' % (self.task)
+
+
+##
+##
+## Controllers
+##
+
+
+# Index of the webapp
+@app.route('/', methods=['GET'])
+def index():
+    return render_template("index.html")
+
+# 404 handling
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
+
+# List items
+@app.route("/tasks", methods=['GET'])
+def get_todos():
+
+    selection = [{'id': x.id,'task': x.task, 'is_done': x.is_done, 'order': x.order}
+                 for x in Item.query.all()]
+    return json.dumps(selection)
+
+
+# Create item
+@app.route("/tasks", methods=['POST'])
+def create_todo():
+
+    if 'task' in request.json:
+
+        # Check order value
+        count = Item.query.count()
+        if 'order' in request.json:
+            order = request.json['order']
+            if order > count:
+                order = count
+        else:
+            order = count
+
+        # Check is_done
+        if 'is_done' in request.json:
+            is_done = request.json['is_done'] == True
+        else:
+            is_done = False
+
+        # Save item
+        todo_item = Item(request.json['task'], is_done, order)
+        db.session.add(todo_item)
+
+        # Re-order items if necessary
+        if order < count:
+            Item.update().values(order=Item.order+1).where(Item.order < order)
+
+        db.session.commit()
+
+        return json.dumps({'id': todo_item.id})
+
+    else:
+        return json.dumps({})
+
+
+# Get a single item
+@app.route("/tasks/<string:todo_id>", methods=['GET'])
+def get_todo(todo_id):
+
+    x = Item.query.filter(id=todo_id)
+
+    if x:
+        return json.dumps({'id': x.id,
+                           'task': x.task,
+                           'is_done': x.is_done,
+                           'order': x.order})
+
+
+# Update a single item
+@app.route("/tasks/<int:todo_id>", methods=['PUT'])
+def update_todo(todo_id):
+
+    x = Item.query.get_or_404(todo_id)
+
+    if 'task' in request.json:
+        x.task = request.json['task']
+
+    if 'order' in request.json:
+        count = Item.query.count()
+        order = request.json['order']
+        if order > count:
+            order = count
+        x.order = order
+
+    if 'is_done' in request.json:
+        x.is_done = request.json['is_done'] == True
+
+    db.session.commit()
+
+    return json.dumps({'id': x.id,
+                       'task': x.task,
+                       'is_done': x.is_done,
+                       'order': x.order})
+
+
+@app.route("/tasks/<int:todo_id>", methods=['DELETE'])
+def delete_todo(todo_id):
+    todo_item = Item.query.get_or_404(todo_id)
+    db.session.delete(todo_item)
+    db.session.commit()
+    return json.dumps(None)
+
+
+
+
+# Build the database:
+db.create_all()
+
+
+##
+##
+## Server
+##
+
+
+app.run(host='0.0.0.0', port=8080, debug=True)
---
+++ static/js/app.js
@@ -0,0 +1,241 @@
+/*global $ */
+/*jshint unused:false */
+var app = app || {};
+var ENTER_KEY = 13;
+var ESC_KEY = 27;
+
+(function () {
+	'use strict';
+
+    //
+	// Model
+	//
+
+	app.Task = Backbone.Model.extend({
+		defaults: {
+			task: '',
+			is_done: false
+		},
+		toggleIsDone: function () {
+			this.save({
+				is_done: !this.get('is_done')
+			});
+		}
+	});
+
+    //
+    // Collection
+	//
+
+	var Tasks = Backbone.Collection.extend({
+		model: app.Task,
+        url: '/tasks',
+		comparator: function (task) {
+			return task.get('order');
+		},
+
+        // Get items left
+        itemsLeft: function () {
+			return this.filter(function(task){
+                return !task.get('is_done');
+            });
+		}
+	});
+
+	app.tasks = new Tasks();
+
+
+
+    //
+    // Task View
+	//
+
+	app.TaskView = Backbone.View.extend({
+
+        // DOM manipulation definition
+		tagName:  'li',
+		template: _.template($('#item-template').html()),
+
+		// DOM events setup
+		events: {
+			'click .toggle': 'toggleIsDone',
+			'dblclick label': 'edit',
+			'keydown .edit': 'saveOrRevert',
+			'blur .edit': 'save'
+		},
+
+		// Model events setup
+		initialize: function () {
+			this.listenTo(this.model, 'change', this.render);
+			this.listenTo(this.model, 'destroy', this.remove);
+		},
+
+		// Re-render the task
+		render: function () {
+
+			this.$el.html(this.template(this.model.toJSON()));
+			this.$el.toggleClass('is_done', this.model.get('is_done'));
+			this.$input = this.$('.edit');
+			return this;
+		},
+
+        // Toggle done/not done
+		toggleIsDone: function () {
+			this.model.toggleIsDone();
+		},
+
+		// Switch to "editing" mode
+		edit: function () {
+			this.$el.addClass('editing');
+			this.$input.focus();
+		},
+
+		// Save and switch back to "listing"` mode
+		save: function () {
+
+			if (!this.$el.hasClass('editing')) {
+				return;
+			}
+
+			var task_text = this.$input.val().trim();
+
+            // Edit ...
+			if (task_text) {
+				this.model.save({ task: task_text });
+                this.model.trigger('change');
+			}
+
+            // ... or delete
+            else {
+				this.clear();
+			}
+
+			this.$el.removeClass('editing');
+		},
+
+		// ENTER to save
+        // ESCAPE to cancel
+		saveOrRevert: function (e) {
+			if (e.which === ENTER_KEY) {
+				this.save();
+			}
+            else if (e.which === ESC_KEY) {
+				this.$el.removeClass('editing');
+			}
+		},
+
+		// Remove the item
+		clear: function () {
+			this.model.destroy();
+		}
+	});
+
+
+
+    //
+    // The Application
+	//
+
+	// Our overall **AppView** is the top-level piece of UI.
+	app.AppView = Backbone.View.extend({
+
+		// Instead of generating a new element, bind to the existing skeleton of
+		// the App already present in the HTML.
+		el: '#tictailtodo',
+
+		// Our template for the line of statistics at the bottom of the app.
+		statsTemplate: _.template($('#stats-template').html()),
+
+		events: {
+			'keypress #new-task': 'createOnEnter',
+			'click #toggle-all': 'toggleAllDone',
+			'click .create-task': 'create'
+		},
+
+
+        // DOM initialization
+		// Collections events setup
+		initialize: function () {
+			this.allCheckbox = this.$('#toggle-all')[0];
+			this.$input = this.$('#new-task');
+			this.$footer = this.$('#footer');
+			this.$main = this.$('#main');
+			this.$list = $('#todo-list');
+
+			this.listenTo(app.tasks, 'add', this.addOne);
+			this.listenTo(app.tasks, 'reset', this.addAll);
+			this.listenTo(app.tasks, 'all', this.render);
+
+			// Suppresses 'add' events with {reset: true} and prevents the app view
+			// from being re-rendered for every model. Only renders when the 'reset'
+			// event is triggered at the end of the fetch.
+			app.tasks.fetch({reset: true});
+		},
+
+		// Re-render statistics
+		render: function () {
+			var itemsLeft = app.tasks.itemsLeft().length;
+
+			if (app.tasks.length) {
+				this.$main.show();
+				this.$footer.show();
+
+				this.$footer.html(this.statsTemplate({
+					itemsLeft: itemsLeft
+				}));
+
+			} else {
+				this.$main.hide();
+				this.$footer.hide();
+			}
+
+		},
+
+		// Add a single task
+		addOne: function (task) {
+			var view = new app.TaskView({ model: task });
+			this.$list.append(view.render().el);
+		},
+
+		// Add all tasks
+		addAll: function () {
+			this.$list.html('');
+			app.tasks.each(this.addOne, this);
+		},
+
+		// Create a new item
+        create: function() {
+			if (this.$input.val().trim()) {
+				app.tasks.create({
+                    task: this.$input.val().trim(),
+                    is_done: false
+                });
+				this.$input.val('');
+			}
+        },
+
+		// Create a new item
+		createOnEnter: function (e) {
+			if (e.which === ENTER_KEY) {
+				this.create();
+			}
+		},
+
+		toggleAllDone: function () {
+			var is_done = this.allCheckbox.checked;
+
+			app.tasks.each(function (task) {
+				task.save({
+					is_done: is_done
+				});
+			});
+		}
+	});
+
+
+	Backbone.history.start();
+
+	// kick things off by creating the `App`
+	new app.AppView();
+
+})();
